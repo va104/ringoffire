585797a92b21796b076e54c03adcd04c03ef6732
Vanessa Würdinger <vanessa.wuerdinger@gmx.de>
2022-11-14 10:25:18 +0100

    added start screen and game start screen

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  constructor() { 
+    
+  }
+  
+  ngOnInit(): void {
+    
+  }
+  
+  pickCard() {
+    //Animation abspielen
+    this.pickCardAnimation = true;
+  }
+
+}
