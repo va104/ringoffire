d6e67854a142d05d85f12e35a3e60905e3b9906e
Vanessa Würdinger <vanessa.wuerdinger@gmx.de>
2022-11-15 13:41:15 +0100

    firebase installed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ring-of-fire-9a0a8',
+    appId: '1:5342307102:web:4f33cc4896108753d69f0f',
+    storageBucket: 'ring-of-fire-9a0a8.appspot.com',
+    apiKey: 'AIzaSyDtk1EjL-8XU9_5AixSmTgzsow84xDo51E',
+    authDomain: 'ring-of-fire-9a0a8.firebaseapp.com',
+    messagingSenderId: '5342307102',
+  },
   production: false
 };
 
